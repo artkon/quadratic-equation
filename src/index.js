ad368687091cc68228bfb9c3f37c4873947aa22c
artkon <artemkonopelko@gmail.com>
2018-09-11 02:23:58 +0300

    Implement solveEquation function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  var split = equation.split(' ');
+
+  //Подготовка индексов
+  var a = +split[0];
+  var firstOp = split[3];
+  var b = +split[4];
+  if(firstOp == '-') { b = -b; };
+  var secondOp = split[7];
+  var c = +split[8];
+  if(secondOp == '-') { c = -c; };
+
+  //Считаем корни
+  var D = Math.pow(b, 2) - 4 * a * c;
+  var x1 = ( -b + Math.sqrt(D) )/(2 * a);
+  var x2 = ( -b - Math.sqrt(D) )/(2 * a);
+
+  var result = [];
+
+  if(x1 > x2){
+    result.push( Math.round(x2) );
+    result.push( Math.round(x1) );  
+  } else {
+    result.push( Math.round(x1) );
+    result.push( Math.round(x2) );
+  }
+
+  return result;
 }
