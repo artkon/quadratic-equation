34630053859fbc98223abe1ea24cdf04755d9297
artkon <artemkonopelko@gmail.com>
2018-10-05 21:27:32 +0300

    refactor

--- src/index.js
+++ src/index.js
@@ -1,29 +1,19 @@
 module.exports = function solveEquation(equation) {
-  var split = equation.split(' ');
+  const split = equation.split(' ');
 
   //Подготовка индексов
-  var a = +split[0];
-  var firstOp = split[3];
-  var b = +split[4];
-  if(firstOp == '-') { b = -b; };
-  var secondOp = split[7];
-  var c = +split[8];
-  if(secondOp == '-') { c = -c; };
+  const a = +split[0];
+  const firstOp = split[3];
+  const b = +split[4] * (firstOp == '+' ? 1 : -1);
+  const secondOp = split[7];
+  const c = +split[8] * (secondOp == '+' ? 1 : -1);;
 
   //Считаем корни
-  var D = Math.pow(b, 2) - 4 * a * c;
-  var x1 = ( -b + Math.sqrt(D) )/(2 * a);
-  var x2 = ( -b - Math.sqrt(D) )/(2 * a);
+  const D = Math.pow(b, 2) - 4*a*c;
+  const x1 = ( -b + Math.sqrt(D) ) / (2 * a);
+  const x2 = ( -b - Math.sqrt(D) ) / (2 * a);
 
-  var result = [];
+  const result = [ Math.round(x1), Math.round(x2) ];
 
-  if(x1 > x2){
-    result.push( Math.round(x2) );
-    result.push( Math.round(x1) );  
-  } else {
-    result.push( Math.round(x1) );
-    result.push( Math.round(x2) );
-  }
-
-  return result;
+  return result.sort( (a, b) => a - b );
 }
